8e0c5cc37a47912e454c5bf4aeacaab39eb737af
choiluca <98502065+choiluca@users.noreply.github.com>
2022-02-14 16:33:54 +0900

    Add files via upload

---
+++ 0214/js/sc.js
@@ -0,0 +1,11 @@
+/* 1. 변수 셋으로 만들기 2. 변수 하나로 만들기
+<div class="box2">
+        <div class="gimpo">김포</div>
+</div>
+*/
+var str = "<table border='1' width='100'>";
+str += "<tr>";
+str += "<td>1</td><td>2</td><td>3</td>";
+str += "</table>";
+
+document.write(str);
